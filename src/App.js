f8ca882199e02b5c86e6bfe81817c5e50c332411
bahaanofal <bahaa2000no@gmail.com>
2021-01-25 23:15:14 +0200

    add singin with google by firebase, and connect with database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.util';
+import { auth, createUserProfileDocument } from './firebase/firebase.util';
 import { Component } from 'react';
 
 class App extends Component {
@@ -18,9 +18,8 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
 	componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user })
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
+      createUserProfileDocument(user);
     });
 	}
 
---
+++ src/firebase/test.js
@@ -0,0 +1,12 @@
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+
+// const firestore = firebase.firestore();
+
+// // عملية انشاء والوصول الى الداتابيز
+// firestore.collection('users').doc('XnipWlNqGC68pImMsTPn').collection('cartItem').doc('6wnChuFfyPcUm2Ik18ia');
+
+// // هادا السطر نفس السطر اللي فوقه لكن باختصار
+// firestore.doc('/users/XnipWlNqGC68pImMsTPn/cartItem/6wnChuFfyPcUm2Ik18ia');
+
+// firestore.collection('/users/XnipWlNqGC68pImMsTPn/cartItem');
